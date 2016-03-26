6f4da40d103ed2e19792183c41aca107ab699a42
felipesantoz <felipe.santoz2013@gmail.com>
2016-03-26 05:32:29 -0400

    Added range sensor

--- app/src/main/java/com/ten/dmitry/locationapp/MainActivity.java
+++ app/src/main/java/com/ten/dmitry/locationapp/MainActivity.java
@@ -152,6 +152,25 @@ public class MainActivity extends AppCompatActivity implements BeaconManager.Mon
 
     @Override
     public void onBeaconsDiscovered(Region region, List<Beacon> list){
+        for(Beacon b : list) {
+            if (b.getMajor() == region.getMajor()) {
+                Log.d(TAG, String.valueOf(calculateAccuracy(b.getMeasuredPower(), b.getRssi())));
+            }
+        }
+    }
 
+    protected static double calculateAccuracy(int txPower, double rssi) {
+        if (rssi == 0) {
+            return -1.0; // if we cannot determine accuracy, return -1.
+        }
+
+        double ratio = rssi*1.0/txPower;
+        if (ratio < 1.0) {
+            return Math.pow(ratio,10);
+        }
+        else {
+            double accuracy =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+            return accuracy;
+        }
     }
 }
