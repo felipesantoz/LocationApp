de1c862627d8a9e8606bb99b311ed8a2cefbb4a1
Dmitry Ten <candalistic@gmail.com>
2016-03-26 02:21:24 -0400

    Now, all of the found beacons are displayed in a list view.

--- app/src/main/java/com/ten/dmitry/locationapp/ChooseBeaconActivity.java
+++ app/src/main/java/com/ten/dmitry/locationapp/ChooseBeaconActivity.java
@@ -1,14 +1,10 @@
 package com.ten.dmitry.locationapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
@@ -16,45 +12,65 @@ import com.estimote.sdk.Region;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-public class ChooseBeaconActivity extends AppCompatActivity implements BeaconManager.RangingListener{
+public class ChooseBeaconActivity extends AppCompatActivity implements BeaconManager.RangingListener {
+    public static String SELECTED_UUID;
+    private BeaconManager beaconManager;
+    private Region region;
+    private List<Beacon> beaconSet;
+    private ArrayAdapter<String> adapter;
 
-    private List<Beacon> beaconList;
-    private List<Button> buttonList;
+    public static final String TAG = "ChooseBeaconActivityTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_choose_beacon);
 
         // find this line inside the `onCreate` method:
-        BeaconManager beaconManager = new BeaconManager(this);
+        beaconManager = new BeaconManager(this);
+        region = new Region("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
         // add this below:
         beaconManager.setRangingListener(this);
+
+        beaconSet = new ArrayList<>();
+        ArrayList<String> beaconsIds = new ArrayList<>();
+        beaconsIds.add("");
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.getHeight();
+        adapter = new ArrayAdapter<>(this, R.layout.list_view_item, beaconsIds);
+        listView.setAdapter(adapter);
     }
 
     @Override
     public void onBeaconsDiscovered(Region region, List<Beacon> list) {
-        buttonList = new ArrayList<>();
-        RelativeLayout myLayout = new RelativeLayout(this);
+        Log.d(TAG, String.valueOf(list.size()));
 
-        for (Beacon b : list){
-            Button btn = new Button(this);
-            btn.setText(b.getProximityUUID().toString());
-            myLayout.addView(btn);
+        for (Beacon b : list) {
+            if (!beaconSet.contains(b)) {
+                adapter.add(String.valueOf(b.getMajor()));
+                beaconSet.add(b);
+            }
         }
 
-        setContentView(myLayout);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        beaconManager.stopRanging(region);
     }
 }
